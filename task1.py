64c3d667fb4e098d08c77a373e79dddc4b0cc561
wuyuze <wuyzhust@163.com>
2014-10-31 12:54:14 +0800

    uniquestudio freshmantask1

--- task1.py
+++ task1.py
@@ -10,6 +10,7 @@ class Getcontent(SGMLParser):
         self.getti = False
         self.gettext = False
         self.getimage =False
+        self.title=''
         self.layer = 0
         self.mark=0
         SGMLParser.reset(self)
@@ -47,6 +48,7 @@ class Getcontent(SGMLParser):
         if self.gettext == True :
             self.content.append('        '+text+'\n')
         if self.getti == True:
+            self.title=text
             self.content.append('                       '+text+'\n')
             self.content.append('================================================================================'+'\n')
 
@@ -65,7 +67,7 @@ class Getcontent(SGMLParser):
             self.content.append('[id:'+str(src)[1:-1]+']'+'\n')
 
     def savetomarkdown(self):
-        f=open('No.'+str(self.mark)+'.md','w')
+        f=open(str(self.title)+'.md','w')
         for i in self.content:
             print i
             f.write(str(i))
@@ -96,14 +98,16 @@ url=('http://www.jianshu.com/collection/fcd7a62be697/top','http://www.jianshu.co
 #os.chdir('/home/wuyuze/python-1/theme1')
 for n in range(3):
     geturl1(url[n])
-    for m in url1:
+    url11=list(set(url1))
+    for m in url11:
         geturl2(str(m))
-    os.mkdir('/home/wuyuze/python-1/theme'+str(n))
-    os.chdir('/home/wuyuze/python-1/theme'+str(n))
+    os.mkdir('/home/wuyuze/python-1/theme'+str(n+1))
+    os.chdir('/home/wuyuze/python-1/theme'+str(n+1))
+    url22= list(set(url2))
     for m in range(100):
-        print url2[m]
+        print url22[m]
         content=Getcontent()
         content.mark=m+1
-        htmlcontent=urllib.urlopen(str(url2[m])).read()
+        htmlcontent=urllib.urlopen(str(url22[m])).read()
         content.feed(htmlcontent)
         content.savetomarkdown()
